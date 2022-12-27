f25c8071038015f9cec14525c9eac93fa8bc8a03
Tanay <daveth1106@gmail.com>
2022-12-27 12:49:10 +0530

    added COMPLETED feature

--- src/App.js
+++ src/App.js
@@ -7,31 +7,54 @@ function App() {
   
   const [todoList, setTodoList] = useState([]);
   const [newTask,setNewTask]= useState("");
+
+  
   const handleChange =(event)=>{
     setNewTask(event.target.value)
   }
+
   const addTask =()=>{
     const task={
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName:newTask,
+      completed:false
       
     }
     
     setTodoList([...todoList,task]);
   }
+
+
   const removeTask=(id)=>{
   setTodoList(todoList.filter((task)=> task.id !== id));
   }
+
+
+  const completedTask=(id)=>{
+    setTodoList(
+      todoList.map((task)=>{
+        if(task.id===id){
+          return {...task,completed:true}
+        }
+        else{
+          return task
+        }
+
+    }))
+  }
+
+  
   return (
     <div className="App">
       <div className="addTask">
         <input onChange={handleChange}></input>
         <button onClick={addTask}>add task</button>
+       
       </div>
      
       <div className="list">
       {todoList.map((task)=>{ 
-        return <Task taskName={task.taskName} id ={task.id} removeTask={removeTask}/>
+        return <Task taskName={task.taskName} id ={task.id} removeTask={removeTask} completed={task.completed} completedTask={completedTask}/>
          
       }
       )
--- src/task.js
+++ src/task.js
@@ -1,8 +1,9 @@
 export const Task =(props)=>{
     return(
-    <div className="list">
+    <div className="list" style={{backgroundColor:props.completed ? "green":"white"}}>
     <h1>{props.taskName}</h1>
     <button onClick={()=>props.removeTask(props.id)}>remove task</button>
+    <button onClick={()=>props.completedTask(props.id)}>Completed</button>
     </div>
     )
 }
