7a2e8687c32bd2f8066b41f47fbc75090c20a335
Tanay <daveth1106@gmail.com>
2022-12-28 11:02:49 +0530

    api example 1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 import './App.css';
+import  Axios  from 'axios';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const[catFact,setCatFact]= useState("")
+
+  const fetchFact=()=>{
+    Axios.get("https://catfact.ninja/fact")
+    .then((res)=>{
+    setCatFact(res.data.fact)})
+
+  }
+
+  useEffect(()=>{
+    fetchFact();
+    },[]) 
+
   return (
     <div className="App">
-      <button>generate cat fact </button>
-      <p></p>
+      <button onClick={fetchFact}>generate cat fact </button>
+      <p>{catFact}</p>
       
     </div>
   );
